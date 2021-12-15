925e1be65b9044fb5b6fc324d3d342a1865c5468
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-15 21:41:39 +0100

    commit

--- piedras.py
+++ piedras.py
@@ -4,24 +4,7 @@ n = randint(10,20)
 NUMERODEPIEDRAS = n
 print("el número de piedras iniciales es {}".format(NUMERODEPIEDRAS))
 
-def quitarpiedras(NUMERODEPIEDRAS, n):
-    while True:
-        piedrasfuera = int(input("ELIGE CUANTAS PIEDRAS QUIERES QUITAR: 2,3 o 5>>> "))
-        if piedrasfuera == 2:
-            NUMERODEPIEDRAS = NUMERODEPIEDRAS - 2
-            print("Tenemos {} piedras".format(NUMERODEPIEDRAS))
-            break
-        elif piedrasfuera == 3:
-            NUMERODEPIEDRAS = NUMERODEPIEDRAS - 3
-            print("Tenemos {} piedras".format(NUMERODEPIEDRAS))
-            break
-        elif piedrasfuera == 5:
-            NUMERODEPIEDRAS = NUMERODEPIEDRAS - 5
-            print("Tenemos {} piedras".format(NUMERODEPIEDRAS))
-            break
-        else:
-            print("GERMANGILIPOLLAS")
-    return NUMERODEPIEDRAS
+
 
 
 turno = "jugador1"
@@ -40,6 +23,7 @@ while NUMERODEPIEDRAS > 0:
             print("Tenemos {} piedras".format(NUMERODEPIEDRAS))
         else:
             print("GERMANGILIPOLLAS")
+            turno = "jugador1"
         turno = "jugador2"
     elif turno == "jugador2":
         print("Es el turno del jugador 2")
@@ -55,9 +39,10 @@ while NUMERODEPIEDRAS > 0:
             print("Tenemos {} piedras".format(NUMERODEPIEDRAS))
         else:
             print("GERMANGILIPOLLAS")
+            turno = "jugador2"
         turno = "jugador1"
 
 if NUMERODEPIEDRAS <= 1 and turno == "jugador2":
-    print("Ha ganado el jugador1")
+    print("Ha ganado el jugador 1")
 elif NUMERODEPIEDRAS <= 1 and turno == "jugador1":
-    print("Ha ganado el jugador2")
+    print("Ha ganado el jugador 2")
